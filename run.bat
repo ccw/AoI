5bd8325a567e2ed968b495cb83a4e9270c5d329c
Ken Chen <kchen@digitalriver.com>
2013-05-14 14:22:10 +0800

    Refine execution script

--- run.bat
+++ run.bat
@@ -1,4 +1,7 @@
-REM todo - to use JAVA_HOME environment
+REM ========================================
+REM = Project Executor on Windows Platform =
+REM = todo - to use JAVA_HOME environment  =
+REM ========================================
 
 javac -d out -sourcepath src src\demo\Main.java
 java -classpath out demo.Main
